a3e743d18a5d4737372a18aabfffde4cd7597cd7
rad304 <y.radouani929@gmail.com>
2020-04-30 22:29:51 +0000

    Add exchange feature and  create exchange page template

---
+++ database/migrations/2020_04_30_143511_create_exchanges_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExchangesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchanges', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->timestamp('accepted_at');
+            $table->timestamp('requested_at');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->integer('my_book')->unsigned();
+            $table->foreign('my_book')->references('id')->on('exemplaires')->onDelete('cascade');
+
+            $table->integer('desired_book')->unsigned();
+            $table->foreign('desired_book')->references('id')->on('exemplaires')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exchanges');
+    }
+}
