4095ba2e43dd704486cac4fe12b68f39d3c5c3a7
ylizma <youssefamzil98@gmail.com>
2020-07-07 20:23:32 +0100

    add RecommandationController

--- app/Http/Controllers/RecommandationController.php
+++ app/Http/Controllers/RecommandationController.php
@@ -7,17 +7,39 @@ use App\Exemplaire;
 use App\Livre;
 use App\Exchange;
 use Illuminate\Support\Facades\DB;
+
 class RecommandationController extends Controller
 {
     function getRecommandedBooks()
     {
         if (auth()->user()) {
-            // DB::table('livres')
-            //     ->join('exemplaires', 'livres.id', '!=', 'exemplaires.livre_id')
-            //     ->join('exchanges', 'exemplaires.id', '=', 'exchanges.desired_book')
-            //     ->where('exchanges.user_id', auth()->user()->id)
-            //     ->select('livres.*')
+            $exchanges = DB::table('exchanges')->select('desired_book')
+                ->where('user_id', 1)->get()->toArray();
+                // return $exchanges;
+            $exemplaires = DB::table('exemplaires')
+                ->select('livre_id')
+                ->whereIn('id', $exchanges)
+                ->orWhere('user_id', 1)
+                ->get();
+                // return $exemplaires->toArray();
+            $books = DB::table('livres')->select('titre')->whereNotIn('id', $exemplaires)->get();
+            return $books;
+            // DB::table('exchanges')->select('desired_book')
+            // ->where('name', '=', 'John')
+            // ->orWhere(function($query)
+            // {
+            //     $query->where('votes', '>', 100)
+            //           ->where('title', '<>', 'Admin');
+            // })
+            // ->get();
+            // return DB::table('livres AS l')->select('l.*')
+            // ->whereNotIn('l.id', DB::table('exemplaires AS e')
+            //     ->select('e.livre_id')
+            //     ->whereIn('e.id',DB::table('exchanges AS x')
+            //         ->select('x.desired_book')->where(['x.user_id'=>1, 'x.status'=> 'accepted'])
+            //         ->get()->toArray()
+            //     )->orWhere('e.user_id',1)->get()->toArray())
             //     ->get();
-        }
+        } else return response()->json(null, 204);
     }
 }
--- database/migrations/2020_02_14_190832_create_livres_table.php
+++ database/migrations/2020_02_14_190832_create_livres_table.php
@@ -21,7 +21,7 @@ class CreateLivresTable extends Migration
             $table->string('auteurs');
             $table->string('isbn');
             $table->text('resume');
-            $table->int('nbrpage');
+            $table->bigInteger('nbrpage')->default(0);
             $table->timestamps();
             $table->bigInteger('categorie_id')->unsigned();
             $table->foreign('categorie_id')->references('id')->on('categories')->onDelete('cascade');
